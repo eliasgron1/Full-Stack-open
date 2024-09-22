e3f77d42900b73d393756d94aceaf547eca9a1b9
eliasgron <eliasgronholm03@gmail.com>
2024-09-22 16:10:51 +0300

    5.18-5.23

--- part5/bloglist-frontend/src/components/ExpandedBlog.jsx
+++ part5/bloglist-frontend/src/components/ExpandedBlog.jsx
@@ -4,7 +4,8 @@ const ExpandedBlog = ({ blog, user, likeHandler, deleteHandler }) => {
   return (
     <div>
       <p>{blog.url}</p>
-      <p>{blog.likes}<button onClick={likeHandler}>like</button></p>
+      <p className="likesCount">{blog.likes}</p>
+      <button onClick={likeHandler}>like</button>
       <p>{blog.user.name}</p>
       {
         blog.user.username === user.username ?
--- part5/playwright-tests/tests/bloglist_app.spec.js
+++ part5/playwright-tests/tests/bloglist_app.spec.js
@@ -2,7 +2,6 @@ const { test, describe, expect, beforeEach } = require('@playwright/test')
 
 describe('Blog Application', () => {
 
-  // this is not working
   beforeEach(async ({ page }) => {
     await page.goto('http://localhost:5173')
   })
@@ -64,5 +63,62 @@ describe('Blog Application', () => {
       const likeButton = await blogElement.getByRole('button', { name: 'like' })
       await likeButton.click()
     })
+
+    test('can delete a blog', async ({ page }) => {
+      const blogElement = await page.locator('text=/new blog made in testing by playwrigthview/i').last()
+      await expect(blogElement).toBeVisible()
+
+      const viewButton = await blogElement.locator('button', { hasText: 'view' })
+      await expect(viewButton).toBeVisible()
+      await viewButton.click()
+
+      page.on('dialog', async (dialog) => {
+        expect(dialog.message()).toEqual('do you want to delete new blog made in testing')
+        await dialog.accept()
+      })
+
+      const deleteButton = await blogElement.getByRole('button', { name: 'delete' })
+      await deleteButton.click()
+
+      await expect(page.getByText('blog deleted')).toBeVisible()
+    })
+
+    test('only creator of blog can see delete button', async ({ page }) => {
+
+      const blogElement = await page.locator('text=/new blog by elias by testerview/i').first()
+      await expect(blogElement).toBeVisible()
+
+      const viewButton = await blogElement.locator('button', { hasText: 'view' })
+      await expect(viewButton).toBeVisible()
+      await viewButton.click()
+
+      const deleteButton = await blogElement.getByRole('button', { name: 'delete' })
+      await expect(deleteButton).toHaveCount(0)
+    })
   })
-})  
+  test('blogs are arranged in order from most to least liked', async ({ page }) => {
+
+    const blogElements = await page.locator('text=/new blog made in testing by playwrigthview/i')
+
+    const blogLikes = [0, 0, 0]
+
+    for (let n = 0; n < 3; n++) {
+      const blogElement = blogElements.nth(n);  // Select the n-th blog element
+      const viewButton = await blogElement.locator('button', { hasText: 'view' })
+      await expect(viewButton).toBeVisible()
+      await viewButton.click()
+
+      const likesCountElement = await blogElement.locator('.likesCount')
+      await expect(likesCountElement).toBeVisible()
+      const likesText = await likesCountElement.textContent()
+
+      const likes = parseInt(likesText)
+      blogLikes[n] = likes
+    }
+
+    console.log(blogLikes[0], blogLikes[1], blogLikes[2])
+    console.log('should be true:', (blogLikes[0] > blogLikes[1]) && (blogLikes[1] > blogLikes[2]))
+    const isDescenting = blogLikes[0] > blogLikes[1] && blogLikes[1] > blogLikes[2];
+    expect(isDescenting).toBe(true)
+  })
+})
